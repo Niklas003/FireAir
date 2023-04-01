15bda3f9195c6dcfdb6c1c668202d0b634ff85e0
Niklas <62644517+Niklas003@users.noreply.github.com>
2023-04-01 14:01:43 +0200

    feat: centered items

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="min-h-screen">
-    <div class="flex justify-center">
-      <div class="bg-red-600 h-1/4 min-h-max w-1/4 rounded-md text-4xl text-white text-center p-10 my-8 font-bold">Neuer Einsatz</div>
-      <div class="bg-transparent border-white border-4 h-1/4 min-h-max w-1/4 rounded-md text-2xl text-white text-center p-8 my-8 font-semibold">Einsatz laden</div>
+  <div>
+    <div class="flex flex-col items-center">
+      <div class="cursor-pointer bg-red-600 h-1/4 min-h-max w-1/4 rounded-md text-4xl text-white text-center p-10 my-8 font-bold">Neuer Einsatz</div>
+      <div class="cursor-pointer bg-transparent border-white border-4 h-1/4 min-h-max w-1/4 rounded-md text-2xl text-white text-center p-8 my-8 font-semibold">Einsatz laden</div>
     </div>
 </div>
 </template>
