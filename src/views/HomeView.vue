2f5934fe433ab5f709892d268c8b569c035313ff
Niklas <62644517+Niklas003@users.noreply.github.com>
2023-04-01 13:54:21 +0200

    feat: didi some start things

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <div class="min-h-screen">
+    <div class="flex justify-center">
+      <div class="bg-red-600 h-1/4 min-h-max w-1/4 rounded-md text-4xl text-white text-center p-10 my-8 font-bold">Neuer Einsatz</div>
+      <div class="bg-transparent border-white border-4 h-1/4 min-h-max w-1/4 rounded-md text-2xl text-white text-center p-8 my-8 font-semibold">Einsatz laden</div>
+    </div>
+</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
 });
 </script>
