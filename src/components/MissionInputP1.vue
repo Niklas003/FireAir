2de8732f172e3670473985127fae846bfc5d7fbf
Niklas <62644517+Niklas003@users.noreply.github.com>
2023-04-02 00:22:09 +0200

    feat: making progress

--- src/components/MissionInputP1.vue
+++ src/components/MissionInputP1.vue
@@ -1,45 +1,24 @@
 <template>
     <div>
         <div class="flex flex-col">
-            <input required placeholder="AGT Überwachung" type="text" class="invalid:bg-red-500/20 outline outline-offset-0 outline-2 outline-red-500 valid:bg-green-500/20 valid:outline-green-500 my-4 mt-8 p-2 rounded-md default:bg-white/70 text-xl text-white"
-            :value="surveillance" @input="e => surveillance = (e?.target as any)?.value">
-            <input required placeholder="Einsatzleitung" type="text" class="invalid:bg-red-500/20 outline outline-offset-0 outline-2 outline-red-500 valid:bg-green-500/20 valid:outline-green-500 my-4 p-2 rounded-md default:bg-white/70 text-xl text-white"
-            :value="management" @input="e => management = (e?.target as any)?.value">
-            <h2 class="text-white text-xl"><b>Datum: </b> {{ dateStr }}</h2>
-            <h2 class="text-white text-xl"><b>Uhrzeit: </b> {{ timeStr }}</h2>
+            <input :placeholder="props.placeholder" type="text" class="invalid:bg-red-500/20 outline outline-offset-0 outline-2 outline-red-500 valid:bg-green-500/20 valid:outline-green-500 my-4 mt-8 p-2 rounded-md default:bg-white/70 text-xl text-white"
+            :value="userInput" @input="e => userInput = (e?.target as any)?.value" @focusout="emitInput()" required>
         </div>
-
-        <div class="flex flex-row mt-8 justify-between">
-            <router-link to="/" class="text-white border-2 border-white p-2 rounded-md">zurück</router-link>
-            <router-link to="/" v-if="surveillance.length > 3 && management.length > 3" class="font-semibold bg-green-500/10 text-green-500 border-2 border-green-500 p-2 rounded-md">Weiter</router-link>
-            <div v-else class="font-semibold bg-white/10 text-white/30 border-2 border-white/10 p-2 rounded-md">Weiter</div>
-        </div> 
     </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, defineEmits } from 'vue';
+import { ref, defineEmits, defineProps } from 'vue';
 
-const emits =  defineEmits(['date', 'time', 'surveillance', 'management']);
+const userInput = ref('');
 
-const dateStr = ref('');
-const timeStr = ref('');
-
-emits("time",timeStr);
-emits("date", dateStr)
-
-const surveillance = ref('');
-const management = ref('');
-
-onMounted(() => {
-    getCurrentDate();
+const emits =  defineEmits(['user-input']);
+const props = defineProps({
+    placeholder: String,
 });
 
-function getCurrentDate(){
-    let dateTime = new Date();
-    dateStr.value = dateTime.toLocaleDateString('de-DE');
-    timeStr.value = dateTime.toLocaleTimeString();
-
+function emitInput() {
+    emits("user-input", userInput.value);    
 }
 
 </script>
--- src/views/NewMission.vue
+++ src/views/NewMission.vue
@@ -1,16 +1,53 @@
 <template>
     <div class="p-12">
         <h1 class="text-4xl text-white mb-12">Neuer Einsatz</h1>
-        <MissionInputP1></MissionInputP1>
+        <MissionInputP1 placeholder="AGT Überwachung" @user-input="(userInput: string) => getEmits(userInput, 0)"></MissionInputP1>
+        <MissionInputP1 placeholder="Einsatzleitung" @user-input="(userInput: string) => getEmits(userInput, 1)"></MissionInputP1>
+        <MissionInputP1 placeholder="Stichwort" @user-input="(userInput: string) => getEmits(userInput, 2)"></MissionInputP1>
+        <MissionInputP1 placeholder="Ort" @user-input="(userInput: string) => getEmits(userInput, 3)"></MissionInputP1>
+
+        <h2 class="text-white text-xl"><b>Datum: </b> {{ dateStr }}</h2>
+        <h2 class="text-white text-xl"><b>Uhrzeit: </b> {{ timeStr }}</h2>
         <p class="text-md text-white">Einsatz ID: #{{generateID()}}</p>
+    </div>
+
+    <div class="flex flex-row justify-between mx-12">
+            <router-link to="/" class="rounded-md p-2 border-2 border-white bg-white/10 text-white text-lg">zurück</router-link>
+            <router-link to="/" v-if="allInputsSet()" class="rounded-md p-2 border-2 border-green-500 bg-green-500/10 text-green-500 text-lg">Weiter</router-link>
+            <button v-else class="rounded-md p-2 border-2 border-green-500/20 bg-green-500/10 text-green-500/10 text-lg">Weiter</button>
     </div>
 </template>
 
 <script setup lang="ts">
 import MissionInputP1 from '@/components/MissionInputP1.vue';
+import { onMounted, ref } from 'vue';
+
+const dateStr = ref('');
+const timeStr = ref('');
+var checkArr = ref<boolean[]>([]);
 
 function generateID(){
     return Date.now()
+}
+
+onMounted(()=>{
+    getCurrentDate();
+});
+
+function getEmits(userInput: string, stringID: number){
+    checkArr.value[stringID] = true;
+}
+
+function allInputsSet(): boolean{
+    
+    return checkArr.value[0] && checkArr.value[1] && checkArr.value[2] && checkArr.value[3]? true: false;
+}
+
+function getCurrentDate(){
+    let dateTime = new Date();
+    dateStr.value = dateTime.toLocaleDateString('de-DE');
+    timeStr.value = dateTime.toLocaleTimeString();
+
 }
 
 </script>
