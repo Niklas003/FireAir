c01de72014e0c5995c80805ea5ba9323d5b49cc6
Niklas <nlabitzke77@gmail.com>
2023-04-07 19:32:26 +0200

    fix: fixed the problem with the input missing last char

--- src/components/ValueInputP1.vue
+++ src/components/ValueInputP1.vue
@@ -4,7 +4,7 @@
             <Transition name="fade" mode="out-in">
                 <label v-if="userInput.length > 0" for="input" class="transition ease-in duration-300 text-white relative -bottom-6 left-2 bg-[#22272A] w-fit p-0.5 border border-green-500 rounded-t-md">{{ props.placeholder }}</label>
             </Transition>
-            <input v-on:input="$emit('user-input', userInput)" id="input" :placeholder="props.placeholder" type="text" class="invalid:bg-red-500/20 outline outline-offset-0 outline-1 outline-red-500 valid:bg-green-500/20 valid:outline-green-500 my-4 mt-6 p-2 rounded-md default:bg-white/70 text-xl text-white"
+            <input v-on:change="emitInput()" v-on:input="emitInput()" id="input" :placeholder="props.placeholder" type="text" class="invalid:bg-red-500/20 outline outline-offset-0 outline-1 outline-red-500 valid:bg-green-500/20 valid:outline-green-500 my-4 mt-6 p-2 rounded-md default:bg-white/70 text-xl text-white"
             :value="userInput" @input="e => userInput = (e?.target as any)?.value" required>
         </div>
     </div>
@@ -13,7 +13,7 @@
 <script setup lang="ts">
 import { ref, defineEmits, defineProps } from 'vue';
 
-const userInput = ref('');
+const userInput = ref("");
 
 const emits =  defineEmits(['user-input']);
 const props = defineProps({
@@ -21,7 +21,6 @@ const props = defineProps({
 });
 
 function emitInput() {
-    console.log(userInput.value)
     if(userInput.value.length > 0)
         emits("user-input", userInput.value);    
 }
