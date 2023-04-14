fc29d8e4bebf5e83577f95983bd9c5977cdc99bf
Niklas <nlabitzke77@gmail.com>
2023-04-14 13:57:43 +0200

    feat: added Mission Data interface

---
+++ src/MissionData.ts
@@ -0,0 +1,9 @@
+interface MissionData {
+    missionID: string,
+    date: string,
+    time: string,
+    surveillance: string,
+    manager: string,
+    missionType: string,
+    missionPlace: string,
+}
