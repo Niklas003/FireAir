dc9c2cf888a93dd22f7e27b5f5983978a1278a11
Niklas <nlabitzke77@gmail.com>
2023-04-14 12:20:37 +0200

    feat: started CheckMissionData

--- src/views/CheckMissionData.vue
+++ src/views/CheckMissionData.vue
@@ -1,9 +1,18 @@
 <template>
     <div class="p-12">
         <router-link to="/newmission" class="hover:underline text-white text-lg"><span class="material-symbols-rounded relative top-0.5 text-sm">arrow_back_ios</span> zurück</router-link>
-        <p>Hello World</p>   
+        <div class="bg-sky-950 p-4 rounded-md">
+            <h1 class="text-white font-bold text-2xl">Einsatzdaten</h1>
+            <p>Datum:</p>
+            <p>Zeit:</p>
+            <p>AGT-ÜberWachung: </p>
+        </div>   
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
+var missionData = ref<string[]>([''])
+
 </script>
