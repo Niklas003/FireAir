f88cfaf8985765c46261bb2af4b25c4a4507a022
Niklas <nlabitzke77@gmail.com>
2023-04-15 16:11:33 +0200

    did some on the check Data Page

--- src/views/CheckMissionData.vue
+++ src/views/CheckMissionData.vue
@@ -2,13 +2,13 @@
     <div class="p-4">
         <router-link to="/newmission" class="hover:underline text-white text-lg"><span class="material-symbols-rounded relative top-0.5 text-sm">arrow_back_ios</span> zurück</router-link>
         <div class="bg-sky-950 p-4 rounded-md mt-8 text-white">
-            <h1 class="font-bold text-2xl">Einsatzdaten #{{ props.missionID }}</h1>
-            <p>Datum: </p>
-            <p>Zeit: </p>
-            <p>AGT-Überwachung: </p>
-            <p>Einsatzleitung: </p>
-            <p>Einsatzstichwort: </p>
-            <p>Einsatzort: </p>
+            <h1 class="font-bold text-2xl">Einsatzdaten <span class="font-light text-xl">#{{ props.missionID }}</span></h1>
+            <p class="py-2">Datum: {{ missionData.date }}</p >
+            <p class="py-2">Zeit: {{ missionData.time }}</p>
+            <p class="py-2">AGT-Überwachung: {{ missionData.surveillance }}</p>
+            <p class="py-2">Einsatzleitung: {{ missionData.manager }}</p>
+            <p class="py-2">Einsatzstichwort: {{ missionData.missionType }}</p>
+            <p class="py-2">Einsatzort: {{ missionData.missionPlace }}</p>
         </div>
         <button class="border-green-500 rounded-md text-green-500 p-2 bg-green-500/10 mt-4">Abschließen</button>   
     </div>
@@ -18,8 +18,8 @@
 import { MissionData } from '@/MissionData';
 import { ref, defineProps, onMounted } from 'vue';
 
-var missionID = ref('');
 var missionData: MissionData;
+var missionID = ref('');
 
 const props = defineProps({
     missionID: String,
@@ -31,11 +31,19 @@ onMounted(() => {
 
 function getMissionData(){
     if(props.missionID === undefined){
-        missionID.value = '[{}]';
+        missionID.value = '';
     }else{
-        missionID.value = props.missionID;
+        if(localStorage.getItem(props.missionID)===null){
+            console.log("Did not found Data");
+        }
+        else{
+            // missionString cannot get null because of the fetch by the if statement
+            let missionString:string = localStorage.getItem(props.missionID);
+            missionData = JSON.parse(missionString);
+            console.log(missionData);
+        }
     }        
-    }
 }
+
 
 </script>
