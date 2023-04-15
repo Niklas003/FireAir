c2378d78f0721bb8724fe7161bac99cc34ba1eb0
Niklas <nlabitzke77@gmail.com>
2023-04-15 14:22:01 +0200

    WIP

--- src/views/CheckMissionData.vue
+++ src/views/CheckMissionData.vue
@@ -2,23 +2,35 @@
     <div class="p-4">
         <router-link to="/newmission" class="hover:underline text-white text-lg"><span class="material-symbols-rounded relative top-0.5 text-sm">arrow_back_ios</span> zurück</router-link>
         <div class="bg-sky-950 p-4 rounded-md mt-8 text-white">
-            <h1 class="font-bold text-2xl">Einsatzdaten</h1>
-            <p>Datum:</p>
-            <p>Zeit:</p>
-            <p>AGT-Überwachung: {{ props.missionID }}</p>
+            <h1 class="font-bold text-2xl">Einsatzdaten #{{ props.missionID }}</h1>
+            <p>Datum: </p>
+            <p>Zeit: </p>
+            <p>AGT-Überwachung: </p>
+            <p>Einsatzleitung: </p>
+            <p>Einsatzstichwort: </p>
+            <p>Einsatzort: </p>
         </div>   
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref, defineProps } from 'vue';
+import { ref, defineProps, onMounted } from 'vue';
 
 const props = defineProps({
     missionID: String,
 });
 
-console.log(props.missionID)
+onMounted(() => {
+    getMissionData();
+});
 
-const missionData = ref<string[]>([''])
+function getMissionData(){
+    var missionIdS = localStorage.getItem("missionID");
+    var parsedIdS = missionIdS!==null? JSON.parse(missionIdS) : JSON.parse("{}");
+    console.log(parsedIdS)
+    for(var i in parsedIdS.data){//this does not work
+        console.log(i);
+    }
+}
 
 </script>
