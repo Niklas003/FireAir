b987332f425677409f23ee417823549b9109b753
Niklas <nlabitzke77@gmail.com>
2023-04-18 19:52:23 +0200

    fix: changed a bit on Header Bar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-[#22272A] py-4 shadow-lg shadow-gray-700 flex justify-between items-baseline px-8 text-white">
-        <span @click="toggleMenu()" class="material-symbols-rounded relative top-0.5 cursor-pointer text-2xl">menu</span>
-        <h1 class="text-2xl font-bold"><span class="material-symbols-rounded relative top-0.5">local_fire_department</span>FireAir</h1>
+        <span @click="toggleMenu()" class="material-symbols-rounded relative top-0.5 cursor-pointer text-3xl">menu</span>
+        <h1 class="text-3xl font-bold"><span class="material-symbols-rounded relative top-0.5">local_fire_department</span>FireAir</h1>
     </div>
     <Transition name="slide-fade">
         <AppMenu v-if="menuToggle" @untoggleMenu="() => toggleMenu()"></AppMenu>
