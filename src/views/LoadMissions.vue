d34c149de3aa986d6aa7f06657b736f1dad643cc
Niklas <62644517+Niklas003@users.noreply.github.com>
2023-04-25 18:55:23 +0200

    getting the mission data

--- src/views/LoadMissions.vue
+++ src/views/LoadMissions.vue
@@ -1,27 +1,28 @@
 <template>
     <div class="mx-8">
         <h1 class="text-4xl text-white my-12">Einsatz laden</h1>
-
-        <table class="table-auto w-full text-white border-collapse border overflow-scroll">
-            <thead class="bg-slate-700">
-                <tr>
-                    <th class="border border-slate-600 py-2 text-xl">ID</th>
-                    <th class="border border-slate-600 text-xl">Datum</th>
-                    <th class="border border-slate-600 text-xl">Uhrzeit</th>
-                    <th class="border border-slate-600 text-xl">Einsatzart</th>
-                    <th class="border border-slate-600 text-xl">Einsatzort</th>
-                </tr>
-            </thead>
-            <tbody class="hover:bg-[#3c454b] cursor-pointer">
-                <tr v-for="(i, index) in missionID" :key="index">
-                    <th class="border border-slate-600 font-normal py-4 text-lg">{{ i }}</th>
-                    <th class="border border-slate-600 font-normal text-lg">Datum</th>
-                    <th class="border border-slate-600 font-normal text-lg">Uhrzeit</th>
-                    <th class="border border-slate-600 font-normal text-lg">Einsatzart</th>
-                    <th class="border border-slate-600 font-normal text-lg">Einsatzort</th>
-                </tr>
-            </tbody>
-        </table>
+            <div class="overflow-x-scroll">
+                <table class="table-auto w-full text-white border-collapse border">
+                    <thead class="bg-slate-700">
+                        <tr>
+                            <th class="border border-slate-600 py-2 text-xl">ID</th>
+                            <th class="border border-slate-600 text-xl">Datum</th>
+                            <th class="border border-slate-600 text-xl">Uhrzeit</th>
+                            <th class="border border-slate-600 text-xl">Einsatzart</th>
+                            <th class="border border-slate-600 text-xl">Einsatzort</th>
+                        </tr>
+                    </thead>
+                    <tbody class="hover:bg-[#3c454b] cursor-pointer">
+                        <tr v-for="(i, index) in missionID" :key="index">
+                            <th class="border border-slate-600 font-normal py-4 text-lg">{{ i }}</th>
+                            <th class="border border-slate-600 font-normal text-lg">{{index}}</th>
+                            <th class="border border-slate-600 font-normal text-lg">Uhrzeit</th>
+                            <th class="border border-slate-600 font-normal text-lg">Einsatzart</th>
+                            <th class="border border-slate-600 font-normal text-lg">Einsatzort</th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
     </div>
 </template>
 <script setup lang="ts">
@@ -30,7 +31,7 @@ import { MissionID } from '@/Interfaces/MissionData';
 import { ref, onMounted } from 'vue';
 
 const missionID = ref<MissionID>();
-const missionDataObj = ref<MissionData>();
+const missionDataObj = ref<MissionData>([]);
 
 onMounted(()=>{
     getMissionIDs();
@@ -43,9 +44,13 @@ function getMissionIDs(){
 }
 
 function getMissionData(){
-    for(var i in missionID.value){
-       missionDataObj.value = JSON.parse(localStorage.getItem(i) || "{}");
+    console.log("this is it "+ missionID.value[0]);
+    for(var i of missionID.value){
+       let missData:MissionData = JSON.parse(localStorage.getItem(i) || "{}");
+       missionDataObj.value.push(missData);       
     }
+    console.log(missionDataObj.value[0].time);
+    
 }
 
 </script>
