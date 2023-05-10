511777b15cdd38a20b1459311ece5fdd6af80920
Niklas <62644517+Niklas003@users.noreply.github.com>
2023-05-10 17:17:41 +0200

    no big motivation today...

--- src/components/ValueInputP1.vue
+++ src/components/ValueInputP1.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <div class="flex flex-col">
-            <span class="p-float-label">
-            <InputText v-on:change="emitInput()" v-on:input="emitInput()" id="input" :placeholder="props.placeholder" 
-            :value="userInput" @input="e => userInput = (e?.target as any)?.value" required />
-            <label for="username">{{props.placeholder}}</label>
+            <span class="p-float-label my-4">
+            <Input class="invalid:bg-red-500/20 outline outline-offset-0 outline-1 outline-red-500 valid:bg-green-500/20 valid:outline-green-500 p-2 rounded-md default:bg-white/70 text-xl text-white" v-on:change="emitInput()" v-on:input="emitInput()" id="input" 
+            :value="userInput" @input="(e:any) => userInput = (e?.target as any)?.value" required />
+            <label for="username text-white">{{props.placeholder}}</label>
         </span>
         </div>
     </div>
@@ -12,7 +12,6 @@
 
 <script setup lang="ts">
 import { ref, defineEmits, defineProps } from 'vue';
-import InputText from 'primevue/inputtext';
 
 const userInput = ref("");
 
